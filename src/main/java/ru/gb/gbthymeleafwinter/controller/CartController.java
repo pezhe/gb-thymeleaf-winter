1d90c62661a731106f0edaa2d1702b3734634a42
pezhe <pezhe@list.ru>
2022-02-16 21:51:57 +0300

    Lesson 8 HW done

---
+++ src/main/java/ru/gb/gbthymeleafwinter/controller/CartController.java
@@ -0,0 +1,36 @@
+package ru.gb.gbthymeleafwinter.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.gb.gbthymeleafwinter.entity.Cart;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+
+    private final Cart cart;
+
+    @GetMapping
+    public String getProductList(Model model) {
+        model.addAttribute("products", cart.getProducts());
+        return "cart-list";
+    }
+
+    @GetMapping("/remove")
+    public String removeProduct(@RequestParam(name = "id") Long id) {
+        cart.removeProduct(id);
+        return "redirect:/cart/";
+    }
+
+    @GetMapping("/add")
+    public String addProduct(@RequestParam(name = "id") Long id) {
+        cart.addProduct(id);
+        return "redirect:/product/all";
+    }
+
+}
---
+++ src/main/java/ru/gb/gbthymeleafwinter/entity/Cart.java
@@ -0,0 +1,30 @@
+package ru.gb.gbthymeleafwinter.entity;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+import ru.gb.gbthymeleafwinter.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@Component
+public class Cart {
+
+    private final List<Product> products = new ArrayList<>();
+    private final ProductService productService;
+
+    public void addProduct(Long id) {
+        if (products.stream().anyMatch((p) -> p.getId().equals(id))) return;
+        products.add(productService.findById(id));
+    }
+
+    public void removeProduct(Long id) {
+        products.stream()
+                .filter((p) -> p.getId().equals(id))
+                .findAny()
+                .ifPresent(products::remove);
+    }
+
+}

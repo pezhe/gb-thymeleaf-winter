6b3b3840d671e45deb6480d8560364862541e4b3
pezhe <pezhe@list.ru>
2022-04-17 18:00:52 +0300

    Lesson 5 base commit (upd)

--- src/main/java/ru/gb/gbthymeleafwinter/GbThymeleafWinterApplication.java
+++ src/main/java/ru/gb/gbthymeleafwinter/GbThymeleafWinterApplication.java
@@ -16,23 +16,6 @@ public class GbThymeleafWinterApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(GbThymeleafWinterApplication.class, args);
 
-        ProductService productService = context.getBean(ProductService.class);
-//
-//        productService.findAll();
-//
-//        final Product product = productService.save(Product.builder()
-//                .cost(new BigDecimal("50.0"))
-//                .status(Status.ACTIVE)
-//                .title("Батон")
-//                .date(LocalDate.now())
-//                .build());
-//
-//        System.out.println("До " + productService.findById(product.getId()));
-//
-//        productService.deleteById(product.getId());
-//        productService.deleteById(product.getId());
-//
-//        System.out.println("После " + productService.findById(product.getId()));
     }
 
 }

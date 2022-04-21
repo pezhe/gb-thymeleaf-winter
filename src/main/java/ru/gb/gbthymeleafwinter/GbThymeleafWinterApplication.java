e33b2d7c29875767d3d24746a96a6842001f4a74
pezhe <pezhe@list.ru>
2022-04-21 17:01:24 +0300

    Lesson 6 HW done

--- src/main/java/ru/gb/gbthymeleafwinter/GbThymeleafWinterApplication.java
+++ src/main/java/ru/gb/gbthymeleafwinter/GbThymeleafWinterApplication.java
@@ -3,12 +3,6 @@ package ru.gb.gbthymeleafwinter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.gb.gbthymeleafwinter.entity.Product;
-import ru.gb.gbthymeleafwinter.entity.enums.Status;
-import ru.gb.gbthymeleafwinter.service.ProductService;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class GbThymeleafWinterApplication {
--- src/main/java/ru/gb/gbthymeleafwinter/controller/CartController.java
+++ src/main/java/ru/gb/gbthymeleafwinter/controller/CartController.java
@@ -6,30 +6,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.gb.gbthymeleafwinter.entity.Cart;
+import ru.gb.gbthymeleafwinter.service.CartService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartController {
 
-    private final Cart cart;
+    private final CartService cartService;
 
     @GetMapping
     public String getProductList(Model model) {
-        model.addAttribute("products", cart.getProducts());
+        model.addAttribute("products", cartService.getProducts());
         return "cart-list";
     }
 
     @GetMapping("/remove")
     public String removeProduct(@RequestParam(name = "id") Long id) {
-        cart.removeProduct(id);
+        cartService.removeProduct(id);
         return "redirect:/cart/";
     }
 
     @GetMapping("/add")
     public String addProduct(@RequestParam(name = "id") Long id) {
-        cart.addProduct(id);
+        cartService.addProduct(id);
         return "redirect:/product/all";
     }
 
